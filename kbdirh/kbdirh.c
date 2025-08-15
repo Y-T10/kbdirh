dd8b04978e3e68a8f0b3cee2be1f4200d8d237f3
Y-T10 <t06t06ca2@gmail.com>
2025-08-15 23:28:43 +0900

    マクロを修正

--- kbdirh/kbdirh.c
+++ kbdirh/kbdirh.c
@@ -9,14 +9,14 @@
 #include <kbd.h>
 
 /* レイアウトデータの属性設定 */
-#if !defined(_WIN64)
-    #error "Target platform is not supported."
+#if defined(_M_IA64)
+    #error "IA64 is not supported."
 #endif
 
-// データセクションを定義する
-#pragma section(".data")
+// 広域変数のデータセクション名を指定する
+#pragma data_seg(".data")
 // キーボードデータの属性マクロを定義する
-#define ATTR_KBD_DATA __declspec(allocate(".data"))
+#define ATTR_KBD_DATA
 
 // スキャンコード、仮想キーコード対応表
 // スキャンコードmに対応する仮想キーコードで、SA2VK[m]を初期化する
